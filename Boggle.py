0d18824ffaf9ffeef22be40a4b385706e91af450
Cyril Danilevski <cydanil@gmail.com>
2013-03-12 21:11:29 +0000

    Update Boggle.py

--- Boggle.py
+++ Boggle.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-#
-#  untitled.py
-#
-#  Copyright 2011 Cyril Danilevski <cydanil@shiftout.net>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#
 
 """
 This is a two step program:
@@ -44,10 +23,6 @@ currently in use and remove them whenever I backtrace.
 
 Once I have generated all the words from the board, I use the prefix
 tree to find and return the existing words.
-
-Here is an example:
-http://www.shiftout.net/~cydanil/cs2011/sample.html
-http://www.shiftout.net/~cydanil/cs2011/bogglesample.dot
 
 The english alphabet is constitued of 26 letters.
 Given that alphabet, each vertex might have up to 26 childs, which in
